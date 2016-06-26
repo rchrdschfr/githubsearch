870fcd02e7679ef8b0b9afac6483ae1f4b8b0212
Richard Schaefer <rchrdschfr@gmail.com>
2016-06-26 13:21:17 -0400

    update constants

--- app/reducers/sorting.js
+++ app/reducers/sorting.js
@@ -1,17 +1,17 @@
 import {
-  CHANGE_SORT_TYPE,
-  CHANGE_SORT_ORDER } from 'types';
+  UPDATE_SORT_TYPE,
+  UPDATE_SORT_ORDER } from 'types';
 
 export default function sorting(state = {
   order: 'desc',
   type: 'relevance'
 }, action = {}) {
   switch (action.type) {
-    case CHANGE_SORT_ORDER:
+    case UPDATE_SORT_ORDER:
       return Object.assign({}, state, {
         order: action.order
       });
-    case CHANGE_SORT_TYPE:
+    case UPDATE_SORT_TYPE:
       return Object.assign({}, state, {
         type: action.sortType
       });
